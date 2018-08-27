fe18096d656730d328c8979b36eea9626ac8e050
htomine <htomine@gmail.com>
2018-08-27 21:18:03 +0900

    Add new images

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass']
       },
+      {
+        test: /\.(jpg|png)$/,
+        loaders: 'file-loader?name=[name].[ext]'
+      },
     ]
   },
 
